5c700159e4eceb4faf3d0cadbe91ce6214279741
Hiroyuki Matsuo <h.matsuo.engineer@gmail.com>
2019-05-05 17:08:19 +0900

    Add `-g` option when set env. variables to work preview feature properly

--- functions/__scheme_get_definition.fish
+++ functions/__scheme_get_definition.fish
@@ -1,60 +1,61 @@
-function scheme_default
+function reset_scheme_to_default
   # Color values from: https://github.com/fish-shell/fish-shell/blob/82052a6cc9fa797070d8945130d45226d5cbc1c5/share/functions/__fish_config_interactive.fish#L51
   # normal
-  set scheme_color_normal normal
+  set -g scheme_color_normal normal
   # commands
-  set scheme_color_command 005fd7
+  set -g scheme_color_command 005fd7
   # quoted blocks of text
-  set scheme_color_quote 999900
+  set -g scheme_color_quote 999900
   # IO redirections
-  set scheme_color_redirection 00afff
+  set -g scheme_color_redirection 00afff
   # process separators like ';' and '&'
-  set scheme_color_end 009900
+  set -g scheme_color_end 009900
   # potential errors
-  set scheme_color_error ff0000
+  set -g scheme_color_error ff0000
   # regular command parameters
-  set scheme_color_param 00afff
+  set -g scheme_color_param 00afff
   # code comments
-  set scheme_color_comment 990000
+  set -g scheme_color_comment 990000
   # parameter expansion operators like '*' and '~'
-  set scheme_color_operator 00a6b2
+  set -g scheme_color_operator 00a6b2
   # character escapes like '\n' and '\x70'
-  set scheme_color_escape 00a6b2
+  set -g scheme_color_escape 00a6b2
   # current working directory in the prompt
-  set scheme_color_cwd green
+  set -g scheme_color_cwd green
   # current working directory in the prompt when superuser
-  set scheme_color_cwd_root red
+  set -g scheme_color_cwd_root red
   # autosuggestions
-  set scheme_color_autosuggestion 555
+  set -g scheme_color_autosuggestion 555
   # current username in the prompt
-  set scheme_color_user brgreen
+  set -g scheme_color_user brgreen
   # current host system in the prompt
-  set scheme_color_host normal
+  set -g scheme_color_host normal
   # '^C' indicator on a canceled command
-  set scheme_color_cancel --reverse
+  set -g scheme_color_cancel --reverse
   # valid path name
-  set scheme_color_valid_path --underline
+  set -g scheme_color_valid_path --underline
   # matching parenthesis
-  set scheme_color_match --background=brblue
+  set -g scheme_color_match --background=brblue
   # selected text (in vi visual mode)
-  set scheme_color_selection white --bold --background=brblack
+  set -g scheme_color_selection white --bold --background=brblack
   # history search matches and the selected pager item (must be a background)
-  set scheme_color_search_match bryellow --background=brblack
+  set -g scheme_color_search_match bryellow --background=brblack
   # Additional for completion pager:
   # prefix string, i.e. the string that is to be completed
-  set scheme_pager_color_prefix white --bold --underline
+  set -g scheme_pager_color_prefix white --bold --underline
   # completion itself
-  set scheme_pager_color_completion normal
+  set -g scheme_pager_color_completion normal
   # completion description
-  set scheme_pager_color_description B3A06D
+  set -g scheme_pager_color_description B3A06D
   # progress bar at the bottom left corner
-  set scheme_pager_color_progress brwhite --background=cyan
+  set -g scheme_pager_color_progress brwhite --background=cyan
   # background color of the every second completion
-  set scheme_pager_color_secondary
+  set -g scheme_pager_color_secondary
   # Additional for some commands:
   # current position out of `cdh` / `dirh` command
-  set scheme_color_history_current --bold
+  set -g scheme_color_history_current --bold
 end
+
 
 function __scheme_get_definition \
   -d 'Get specified color scheme definition' \
@@ -71,7 +72,7 @@ function __scheme_get_definition \
   switch $scheme_name
 
     case 'default'
-      scheme_default
+      reset_scheme_to_default
 
     # https://draculatheme.com
     case 'dracula'
@@ -89,34 +90,34 @@ function __scheme_get_definition \
       set -l red            ff5555
       set -l yellow         f1fa8c
       # Preferred to reset all colors
-      scheme_default
+      reset_scheme_to_default
       # Color configurations
-      # set scheme_color_normal             normal
-      set scheme_color_command            $foreground
-      set scheme_color_quote              $yellow
-      set scheme_color_redirection        $cyan
-      set scheme_color_end                $green
-      set scheme_color_error              $red
-      set scheme_color_param              $pink
-      set scheme_color_comment            $comment
-      set scheme_color_operator           $green
-      set scheme_color_escape             $purple
-      set scheme_color_cwd                $green
-      set scheme_color_cwd_root           $red
-      set scheme_color_autosuggestion     $purple
-      set scheme_color_user               $orange
-      set scheme_color_host               $foreground
-      # set scheme_color_cancel             --reverse
-      # set scheme_color_valid_path         --underline
-      # set scheme_color_match              --background=brblue
-      # set scheme_color_selection          white --bold --background=brblack
-      # set scheme_color_search_match       bryellow --background=brblack
-      # set scheme_pager_color_prefix       white --bold --underline
-      # set scheme_pager_color_completion   normal
-      # set scheme_pager_color_description  B3A06D
-      # set scheme_pager_color_progress     brwhite --background=cyan
-      # set scheme_pager_color_secondary
-      # set scheme_color_history_current    --bold
+      # set -g scheme_color_normal             normal
+      set -g scheme_color_command            $foreground
+      set -g scheme_color_quote              $yellow
+      set -g scheme_color_redirection        $cyan
+      set -g scheme_color_end                $green
+      set -g scheme_color_error              $red
+      set -g scheme_color_param              $pink
+      set -g scheme_color_comment            $comment
+      set -g scheme_color_operator           $green
+      set -g scheme_color_escape             $purple
+      set -g scheme_color_cwd                $green
+      set -g scheme_color_cwd_root           $red
+      set -g scheme_color_autosuggestion     $purple
+      set -g scheme_color_user               $orange
+      set -g scheme_color_host               $foreground
+      # set -g scheme_color_cancel             --reverse
+      # set -g scheme_color_valid_path         --underline
+      # set -g scheme_color_match              --background=brblue
+      # set -g scheme_color_selection          white --bold --background=brblack
+      # set -g scheme_color_search_match       bryellow --background=brblack
+      # set -g scheme_pager_color_prefix       white --bold --underline
+      # set -g scheme_pager_color_completion   normal
+      # set -g scheme_pager_color_description  B3A06D
+      # set -g scheme_pager_color_progress     brwhite --background=cyan
+      # set -g scheme_pager_color_secondary
+      # set -g scheme_color_history_current    --bold
 
     case 'monokai'
       # Color palette
@@ -130,34 +131,34 @@ function __scheme_get_definition \
       set -l red            f92672
       set -l yellow         e6db74
       # Preferred to reset all colors
-      scheme_default
+      reset_scheme_to_default
       # Color configurations
-      # set scheme_color_normal             normal
-      set scheme_color_command            $cyan
-      set scheme_color_quote              $yellow
-      set scheme_color_redirection        $orange
-      set scheme_color_end                $red
-      set scheme_color_error              $red
-      set scheme_color_param              $foreground
-      set scheme_color_comment            $comment
-      set scheme_color_operator           $foreground
-      set scheme_color_escape             $purple
-      set scheme_color_cwd                $green
-      set scheme_color_cwd_root           $red
-      set scheme_color_autosuggestion     $comment
-      set scheme_color_user               $orange
-      set scheme_color_host               $foreground
-      # set scheme_color_cancel             --reverse
-      # set scheme_color_valid_path         --underline
-      # set scheme_color_match              --background=brblue
-      # set scheme_color_selection          white --bold --background=brblack
-      # set scheme_color_search_match       bryellow --background=brblack
-      # set scheme_pager_color_prefix       white --bold --underline
-      # set scheme_pager_color_completion   normal
-      # set scheme_pager_color_description  B3A06D
-      # set scheme_pager_color_progress     brwhite --background=cyan
-      # set scheme_pager_color_secondary
-      # set scheme_color_history_current    --bold
+      # set -g scheme_color_normal             normal
+      set -g scheme_color_command            $cyan
+      set -g scheme_color_quote              $yellow
+      set -g scheme_color_redirection        $orange
+      set -g scheme_color_end                $red
+      set -g scheme_color_error              $red
+      set -g scheme_color_param              $foreground
+      set -g scheme_color_comment            $comment
+      set -g scheme_color_operator           $foreground
+      set -g scheme_color_escape             $purple
+      set -g scheme_color_cwd                $green
+      set -g scheme_color_cwd_root           $red
+      set -g scheme_color_autosuggestion     $comment
+      set -g scheme_color_user               $orange
+      set -g scheme_color_host               $foreground
+      # set -g scheme_color_cancel             --reverse
+      # set -g scheme_color_valid_path         --underline
+      # set -g scheme_color_match              --background=brblue
+      # set -g scheme_color_selection          white --bold --background=brblack
+      # set -g scheme_color_search_match       bryellow --background=brblack
+      # set -g scheme_pager_color_prefix       white --bold --underline
+      # set -g scheme_pager_color_completion   normal
+      # set -g scheme_pager_color_description  B3A06D
+      # set -g scheme_pager_color_progress     brwhite --background=cyan
+      # set -g scheme_pager_color_secondary
+      # set -g scheme_color_history_current    --bold
 
     case '*'
       echo (set_color normal)(set_color $fish_color_error)(set_color -o)'Error:'(set_color normal)' '(set_color $fish_color_normal)'No such scheme available:'(set_color normal)' '(set_color $fish_color_error)$scheme_name(set_color normal)
