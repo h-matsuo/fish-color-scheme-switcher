1cbbc91aad6c82c40053f19fb59ae9cdeabc02ac
Hiroyuki Matsuo <h.matsuo.engineer@gmail.com>
2019-05-05 10:27:31 +0900

    Update function calling

--- functions/__scheme_list.fish
+++ functions/__scheme_list.fish
@@ -1,15 +1,6 @@
 function __scheme_list \
   -d 'Get list of available color schemes'
 
-  set -l repo_path "$HOME/.config/fish/functions/"
-  set -l scheme_def_filename_suffix '__scheme_def_'
-  set -l scheme_def_filename_postfix '.fish'
-
-  ls $repo_path \
-    | grep "$scheme_def_filename_suffix" \
-    | grep "$scheme_def_filename_postfix" \
-    | sed \
-      -e "s/$scheme_def_filename_suffix//" \
-      -e "s/$scheme_def_filename_postfix//"
+  __scheme_get_definition
 
 end
--- functions/__scheme_set.fish
+++ functions/__scheme_set.fish
@@ -1,35 +1,39 @@
 function __scheme_set \
-  -d 'Set color scheme to specified one' \
+  -d 'Set current color scheme to specified one' \
   -a scheme_name
-
-  set -l repo_path "$HOME/.config/fish/functions/"
-  set -l scheme_def_filename_suffix '__scheme_def_'
-  set -l scheme_def_filename_postfix '.fish'
-
-
-  # Validate provided scheme name
 
   if [ -z $scheme_name ]
     echo (set_color -o $fish_color_error)'Error'(set_color $fish_color_normal)': Scheme name not provided.'
     return 1
   end
 
-  set -l matched (__scheme_get_list | grep -w "$scheme_name")
-  if [ -z $matched ]
-    echo (set_color -o $fish_color_error)'Error: '(set_color $fish_color_normal)'No such scheme available: '(set_color $fish_color_error)$scheme_name
-    set_color $fish_color_normal
-    return 1
-  end
+  __scheme_get_definition "$scheme_name"
 
-
-  # Load scheme definition
-
-  # set -l scheme_def_file_path "$HOME/.config/fish/functions/$scheme_def_filename_suffix$matched$scheme_def_filename_postfix"
-
-  # source "$scheme_def_file_path"
-
-  set -l scheme_def_func "$scheme_def_filename_suffix$matched"
-
-  "$scheme_def_func"
+  set -xg fish_color_normal             $scheme_color_normal
+  set -xg fish_color_command            $scheme_color_command
+  set -xg fish_color_quote              $scheme_color_quote
+  set -xg fish_color_redirection        $scheme_color_redirection
+  set -xg fish_color_end                $scheme_color_end
+  set -xg fish_color_error              $scheme_color_error
+  set -xg fish_color_param              $scheme_color_param
+  set -xg fish_color_comment            $scheme_color_comment
+  set -xg fish_color_operator           $scheme_color_operator
+  set -xg fish_color_escape             $scheme_color_escape
+  set -xg fish_color_cwd                $scheme_color_cwd
+  set -xg fish_color_cwd_root           $scheme_color_cwd_root
+  set -xg fish_color_autosuggestion     $scheme_color_autosuggestion
+  set -xg fish_color_user               $scheme_color_user
+  set -xg fish_color_host               $scheme_color_host
+  set -xg fish_color_cancel             $scheme_color_cancel
+  set -xg fish_color_valid_path         $scheme_color_valid_path
+  set -xg fish_color_match              $scheme_color_match
+  set -xg fish_color_selection          $scheme_color_selection
+  set -xg fish_color_search_match       $scheme_color_search_match
+  set -xg fish_pager_color_prefix       $scheme_pager_color_prefix
+  set -xg fish_pager_color_completion   $scheme_pager_color_completion
+  set -xg fish_pager_color_description  $scheme_pager_color_description
+  set -xg fish_pager_color_progress     $scheme_pager_color_progress
+  set -xg fish_pager_color_secondary    $scheme_pager_color_secondary
+  set -xg fish_color_history_current    $scheme_color_history_current
 
 end
